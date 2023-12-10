036c4473cafcd7d19fac793b418dd981ae47f87a
videoaudiodisco <gyungyoonpark@gmail.com>
2023-12-10 15:00:56 +0900

    file modified

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,10 +1,6 @@
 FRUIT=$1
-if [$FRUIT == APPLE];then
-    echo "You selected Apple!"
-elif [$FRUIT == ORANGE];then
-    echo "you selected Orange!"
-elif [$FRUIT ==GRAPE];then
-    echo "You selected Grape!"
-else
-    echo "You selected other fruits!"
+if [ $FRUIT == APPLE ];then echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then echo "you selected Orange!"
+elif [ $FRUIT == GRAPE ];then echo "You selected Grape!"
+else echo "You selected other fruits!"
 fi
