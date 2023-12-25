9c9dbf01010a63c6fb22be747f602dd040c83465
videoaudiodisco <gyungyoonpark@gmail.com>
2023-12-25 16:57:07 +0900

    mod1

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -11,7 +11,7 @@ with DAG(
 ) as dag:
     
     bash_push=BashOperator(
-        task_id = 'bash_push',
+        task_id='bash_push',
         bash_command="echo START &&"
                         "echo XCOM_PUSHED"
                         "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
