f90aea5cfdb8d85dc9810a4ec47c3d964c0b07b8
videoaudiodisco <gyungyoonpark@gmail.com>
2023-12-28 23:39:40 +0900

    slkdsf

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -43,23 +43,23 @@ class SeoulApiToCsvOperator(BaseOperator):
             os.system(f'mkdir -p {self.path}')
         total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
 
-        def _call_api(self, base_url, start_row, end_row):
-            import requests
-            import json
+    def _call_api(self, base_url, start_row, end_row):
+        import requests
+        import json
 
-            headers = {'Content-Type': 'application/json',
-                   'charset': 'utf-8',
-                   'Accept': '*/*'
-                   }
-            
-            request_url = f'{base_url}/{start_row}/{end_row}/'
-            if self.base_dt is not None:
-                request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
-            response=requests.get(request_url, headers)
-            contents=json.loads(response.text)
+        headers = {'Content-Type': 'application/json',
+                'charset': 'utf-8',
+                'Accept': '*/*'
+                }
+        
+        request_url = f'{base_url}/{start_row}/{end_row}/'
+        if self.base_dt is not None:
+            request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
+        response=requests.get(request_url, headers)
+        contents=json.loads(response.text)
 
-            key_nm = list(contents.keys())[0]
-            row_data=contents.get(key_nm).get('row')
-            row_df=pd.DataFrame(row_data)
+        key_nm = list(contents.keys())[0]
+        row_data=contents.get(key_nm).get('row')
+        row_df=pd.DataFrame(row_data)
 
-            return row_df
+        return row_df
