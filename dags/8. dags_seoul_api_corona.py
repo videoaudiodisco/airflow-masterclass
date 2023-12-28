8aa2aa77897f12a14e9cdd2ec28c0d5d4f46512d
videoaudiodisco <gyungyoonpark@gmail.com>
2023-12-29 07:37:57 +0900

    sldkf

--- dags/8. dags_seoul_api_corona.py
+++ dags/8. dags_seoul_api_corona.py
@@ -11,8 +11,8 @@ with DAG(
     '''서울시 코로나19 확진자 발생동향'''
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id='tb_corona19_count_status',
-        # dataset_nm='TbCorona19CountStatus',
-        dataset_nm='TbUseDaystatusView',
+        dataset_nm='TbCorona19CountStatus',
+        # dataset_nm='TbUseDaystatusView',
         path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='TbCorona19CountStatus.csv'
     )
@@ -20,8 +20,8 @@ with DAG(
     '''서울시 코로나19 백신 예방접종 현황'''
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
         task_id='tv_corona19_vaccine_stat_new',
-        # dataset_nm='tvCorona19VaccinestatNew',
-        dataset_nm='TbUseDaystatusView',
+        dataset_nm='tvCorona19VaccinestatNew',
+        # dataset_nm='TbUseDaystatusView',
         path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='tvCorona19VaccinestatNew.csv'
     )
