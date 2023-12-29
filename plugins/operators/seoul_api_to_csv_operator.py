3a5511d0a2cca1ebd9e1cd2756af610de92cdf1e
videoaudiodisco <gyungyoonpark@gmail.com>
2023-12-30 00:50:53 +0900

    check8

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -58,8 +58,7 @@ class SeoulApiToCsvOperator(BaseOperator):
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
         response = requests.get(request_url, headers)
         print('response', response)
-        # contents = json.loads(response.text)
-        contents = json.loads(response)
+        contents = json.loads(response.text)
         print('contents', contents)
 
         key_nm = list(contents.keys())[0]
