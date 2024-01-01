ef349eb027b2b6ea6f53603c022151473ea9fa31
videoaudiodisco <gyungyoonpark@gmail.com>
2024-01-01 19:54:35 +0900

    test1

--- dags/9. dags_python_with_postgres.py
+++ dags/9. dags_python_with_postgres.py
@@ -48,28 +48,22 @@ with DAG(
         import psycopg2
         from contextlib import closing
 
-        print('lalala')
-
         with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
-            print('conn', conn)
             with closing(conn.cursor()) as cursor:
-                print('cursor', cursor)
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
                 msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
-                print('실행1')
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
-                print('실행2')
                 conn.commit()
-                print('실행3')
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5432', 'gypark', 'gypark', 'gypark']
+        # op_args=['172.28.0.3', '5432', 'gypark', 'gypark', 'gypark']
+        op_args=['localhost', '5555', 'gypark', 'gypark', 'gypark']
+
     )
-    print('실행4')
         
     insrt_postgres
