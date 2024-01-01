af2cd9435706a7c26d42016befb5330835545420
videoaudiodisco <gyungyoonpark@gmail.com>
2024-01-01 19:17:41 +0900

    sldkfjsldf

--- dags/9. dags_python_with_postgres.py
+++ dags/9. dags_python_with_postgres.py
@@ -68,7 +68,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5429', 'gypark', 'gypark', 'gypark']
+        op_args=['172.28.0.3', '5432', 'gypark', 'gypark', 'gypark']
     )
     print('실행4')
         
